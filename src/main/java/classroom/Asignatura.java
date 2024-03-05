82f892b73bc1eef2270e4360ea72d2e9c56800cf
Juan Villa <jujotovil@gmail.com>
2024-03-04 23:26:45 -0500

    Fix

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -9,16 +9,16 @@ public class Asignatura {
     public Tipo tipo;
 
     public Asignatura() {
-        this("Sin nombre",Tipo.FUNDAMENTACION);
-        this(0);
-    }
-
-    public Asignatura(int codigoInterno,Tipo tipo) {
-        this("Sin nombre", codigoInterno, 0,tipo);
+        this(0,Tipo.FUNDAMENTACION);
+        //this(0);
     }
 
     public Asignatura(int codigoExterno,Tipo tipo) {
-        this("Sin nombre", 0, codigoExterno,tipo);
+        this("Sin nombre",0, codigoExterno,tipo);
+    }
+
+    public Asignatura(int codigoInterno, int codigoExterno,Tipo tipo) {
+        this("Sin nombre", codigoInterno, codigoExterno,tipo);
     }
 
     public Asignatura(String nombre, Tipo tipo) {
@@ -39,8 +39,8 @@ public class Asignatura {
         this.tipo = tipo;
     }
 
-    public void cambiarDatos(int codigoInterno) {
-        this.codigoInterno = codigoInterno;
+    public void cambiarDatos(double codigoInterno) {
+        this.codigoInterno = (int)codigoInterno;
     }
 
     public void cambiarDatos(int codigoExterno) {
